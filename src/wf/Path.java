7ad26876d273e54ee465fea5fb2c1a5176f5030e
vadimdubka <vadimdubka@gmail.com>
2018-03-18 22:03:57 +0300

    "Path" task is done.

--- src/wf/Path.java
+++ src/wf/Path.java
@@ -1,28 +1,27 @@
 package wf;
 
-import java.util.TreeSet;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/*https://www.testdome.com/d/java-interview-questions/4 #8*/
 public class Path {
-    private String  path;
-    private TreeSet folders;
+    private       String        path;
+    private final Deque<String> pathDeck;
     
-    private static final String EMAIL_REGEX    =
-        "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
-    private static final String PASSWORD_REGEX =
-        "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[\\w_-]{8,}$";
+    private final String separator = "/";
+    private final String parentDir = "..";
     
-    private String folderNamePattern         = "\\w+";
-    private String pathSeparatorPattern      = "\\/";
-    private String moveToParentFolderPattern = "\\.\\.";
-    
-    //    private String move = "(/|\\w+|\\.\\.)";
-    private String move = "(/|([a-zA-Z]+)|\\.\\.)";
-    Pattern movePattern = Pattern.compile(move);
+    private String  move        = "(/|([a-zA-Z]+)|\\.\\.)";
+    private Pattern movePattern = Pattern.compile(move);
     
     public Path(String path) {
         this.path = path;
+        String[] arr = path.split("/");
+        pathDeck = new ArrayDeque<>(Arrays.asList(arr));
+        pathDeck.removeFirst();
     }
     
     public String getPath() {
@@ -30,27 +29,41 @@ public class Path {
     }
     
     public void cd(String newPath) {
-        Matcher moveMatcher = movePattern.matcher(newPath);
-        int     start       = 0;
-        int     end         = 0;
-        while (moveMatcher.find(end)) {
-            start = moveMatcher.start();
-            end = moveMatcher.end();
-            System.out.println(start + " " + end + " " + newPath.substring(start, end));
+        if (separator.equals(newPath.substring(0, 1))) {
+            path = newPath;
+        } else {
+            Matcher moveMatcher = movePattern.matcher(newPath);
+            int end = 0;
+            while (moveMatcher.find(end)) {
+                int start = moveMatcher.start();
+                end = moveMatcher.end();
+                String moveStr = newPath.substring(start, end);
+                makeMove(moveStr);
+            }
+            buildPath();
         }
     }
     
-    private static boolean isMatchPattern(String str, String regex) {
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(str);
-        return matcher.matches();
+    private void buildPath() {
+        StringBuilder pathBuilder = new StringBuilder(pathDeck.size() * 2);
+        for (String s : pathDeck) {
+            pathBuilder.append(separator);
+            pathBuilder.append(s);
+        }
+        path = pathBuilder.toString();
+    }
+    
+    private void makeMove(String moveStr) {
+        if (moveStr.equals(parentDir)) {
+            pathDeck.removeLast();
+        } else if (!moveStr.equals(separator)) {
+            pathDeck.addLast(moveStr);
+        }
     }
     
     public static void main(String[] args) {
-//        Path path = new Path("/a/b/c/d");
-        Path path = new Path("/a/b/c/d/a/kb/c/d");
-//        path.cd("../x");
-        path.cd("/as../kb/c/d/a/b/c/d");
+        Path path = new Path("/a/b/c/d");
+        path.cd("../x");
         System.out.println(path.getPath());
     }
 }
